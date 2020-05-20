3a1e5108aea9b5cd88d7dff0d9314fc610db247e
swhui <42617476+swhui@users.noreply.github.com>
2020-05-20 11:41:26 -0700

    Add files via upload

---
+++ lab11/lab11.sql
@@ -0,0 +1,22 @@
+.read sp20data.sql
+
+CREATE TABLE obedience AS
+  SELECT seven, instructor FROM students;
+
+CREATE TABLE smallest_int AS
+  SELECT time, smallest FROM students WHERE smallest > 2 ORDER BY smallest LIMIT 20;
+
+CREATE TABLE matchmaker AS
+  SELECT a.pet, a.song, a.color, b.color
+  FROM students AS a, students AS b
+  WHERE a.pet=b.pet AND a.song=b.song AND a.time < b.time;
+
+
+-- Ways to stack 4 dogs to a height of at least 170, ordered by total height
+CREATE TABLE stacks_helper(dogs, stack_height, last_height);
+
+-- Add your INSERT INTOs here
+
+
+CREATE TABLE stacks AS
+  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
